bb2d41d159872c8c639ccd0930dc76d3e9951e40
Gustavo Abreu <gustavofart371@gmail.com>
2022-05-24 17:11:14 -0300

    Cria uma página de listagem de produtos vazia

---
+++ src/components/Search.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Search extends React.Component{
+    render() {
+        return (
+            <div>
+                <label>
+                <input 
+                />
+                </label>
+                <h2 data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</h2>
+            </div>
+        )
+    }
+}
+
+export default Search;
